18a510588f4421c5edef5a3c9079cb5487638dd4
hieu1509 <hhieu1592004@gmail.com>
2024-11-28 02:05:37 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,7 +43,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
 
-        // Tạo các vai trò và phân quyền
+        // // Tạo các vai trò và phân quyền
         $adminRole = Role::firstOrCreate(['name' => 'admin']);
         Permission::firstOrCreate(['name' => 'access admin panel']);
 
